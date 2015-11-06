e23aec8815700d8f3a727c7c358a85c885a5cc38
Whipple Walter <whipnwalt@gmail.com>
2015-11-06 11:16:01 -0700

    Start Project

---
+++ hackUstate/hackUstate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hackUstate
+//
+//  Created by Melissa Walter on 11/6/15.
+//  Copyright © 2015 pacumenigames. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
